fcd0554e89f65666d138105408db58985fcbbf2a
Guillermo <guillermo.lopez@iesdonana.org>
2016-02-08 16:16:32 +0100

    Tabla usuarios y ci_sessions hecha en bd.sql.

---
+++ application/sql/bd.sql
@@ -0,0 +1,21 @@
+drop table if exists usuarios;
+
+create table usuarios(
+    id bigserial constraint pk_usuarios primary key,
+    nick varchar(100) not null constraint uq_usuarios_nick unique,
+    password char(60) not null contraint ck_password_valida
+                               check (length(password) = 60),
+    email varchar(100) not null
+);
+
+
+drop table if exists ci_sessions cascade;
+
+create table "ci_sessions" (
+    "id" varchar(40) not null primary key,
+    "ip_address" varchar(45) not null,
+    "timestamp" bigint default 0 not null,
+    "data" text default '' not null
+);
+
+create index "ci_sessions_timestamp" on "ci_sessions" ("timestamp");
