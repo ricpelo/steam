07eb297744a73d69a6e54919807afb9ca2c339b0
Mario Sumariva <mariosumd@gmail.com>
2016-02-09 20:05:08 +0100

    Sistema de valoraciones en el modelo Juego añadido

--- application/controllers/Juegos.php
+++ application/controllers/Juegos.php
@@ -22,50 +22,21 @@ class Juegos extends CI_Controller {
             'field' => 'precio',
             'label' => 'Precio',
             'rules' => 'trim|required|numeric|less_than_equal_to[9999.99]'
+        ),
+        array(
+            'field' => 'existencias',
+            'label' => 'Existencias',
+            'rules' => 'trim|integer|greater_than_equal_to[-2147483648]|less_than_equal_to[+2147483647]'
         )
     );
 
-    public function index() 
+    public function index()
     {
         $this->output->cache(1);
         $data['filas'] = $this->Juego->todos();
         $this->template->load('juegos/index', $data, array('title' => 'Listado de juegos'));
     }
 
-    public function borrar($id = NULL)
-    {
-        if ($this->input->post('borrar') !== NULL)
-        {
-            $id = $this->input->post('id');
-            if ($id !== NULL)
-            {
-                $this->Juego->borrar($id);
-                $this->output->delete_cache('/juegos/index');
-            }
-            redirect('juegos/index');
-        }
-        else
-        {
-            if ($id === NULL)
-            {
-                redirect('juegos/index');
-            }
-            else
-            {
-                $res = $this->Juego->por_id($id);
-                if ($res === FALSE)
-                {
-                    redirect('juegos/index');
-                }
-                else
-                {
-                    $data = $res;
-                    $this->template->load('juegos/borrar', $data);
-                }
-            }
-        }
-    }
-    
     public function insertar()
     {
         if ($this->input->post('insertar') !== NULL)
@@ -83,13 +54,7 @@ class Juegos extends CI_Controller {
         }
         $this->template->load('juegos/insertar');
     }
-    
-    private function limpiar($accion, $valores)
-    {
-        unset($valores[$accion]);
-        return $valores;
-    }
-    
+
     public function editar($id = NULL)
     {
         if ($id === NULL)
@@ -112,31 +77,13 @@ class Juegos extends CI_Controller {
                 redirect('juegos/index');
             }
         }
-        $valores = $this->Juego->por_id($id);
+        $valores = $this->Articulo->por_id($id);
         if ($valores === FALSE)
         {
-            redirect('juegos/index');
+            redirect('articulos/index');
         }
         $data = $valores;
-        $this->template->load('juegos/editar', $data);
+        $this->template->load('articulos/editar', $data);
     }
-    
-    public function _codigo_unico($codigo, $id)
-    {
-        $res = $this->Juego->por_codigo($codigo);
 
-        if ($res === FALSE || $res['id'] === $id)
-        {
-            return TRUE;
-        }
-        else
-        {
-            $this->form_validation->set_message('_codigo_unico',
-                'El {field} debe ser único.');
-            return FALSE;
-        }
-    }
-    
-    
-    
 }
