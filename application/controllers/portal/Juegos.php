0515c21d0ca8f5d631a88a700b6cde865b722399
Sergio Alcántara <sergio.alcantara.1992@gmail.com>
2016-02-15 16:33:06 +0100

    Cambio en el diseño de juegos, actualización de la sql juegos y diferencia entre usuario y administrador

---
+++ application/controllers/portal/Juegos.php
@@ -0,0 +1,118 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Juegos extends CI_Controller {
+
+    private $reglas_comunes = array(
+        array(
+            'field' => 'descripcion',
+            'label' => 'Descripción',
+            'rules' => 'trim|required|max_length[50]'
+        ),
+        array(
+            'field' => 'precio',
+            'label' => 'Precio',
+            'rules' => 'trim|required|numeric|less_than_equal_to[9999.99]'
+        ),
+        array(
+            'field' => 'existencias',
+            'label' => 'Existencias',
+            'rules' => 'trim|integer|greater_than_equal_to[-2147483648]|less_than_equal_to[+2147483647]'
+        )
+    );
+
+    public function index()
+    {
+        $this->output->cache(1);
+        $data['filas'] = $this->Juego->todos();
+        $this->template->load('juegos/index', $data, array('title' => 'Listado de juegos'));
+    }
+    
+    public function borrar($id = NULL)
+    {
+        if ($this->input->post('borrar') !== NULL)
+        {
+            $id = $this->input->post('id');
+            if ($id !== NULL)
+            {
+                $this->Juego->borrar($id);
+                $this->output->delete_cache('/juegos/index');
+            }
+            redirect('juegos/index');
+        }
+        else
+        {
+            if ($id === NULL)
+            {
+                redirect('juegos/index');
+            }
+            else
+            {
+                $res = $this->Juego->por_id($id);
+                if ($res === FALSE)
+                {
+                    redirect('juegos/index');
+                }
+                else
+                {
+                    $data = $res;
+                    $this->template->load('juegos/borrar', $data);
+                }
+            }
+        }
+    }
+    
+    public function insertar()
+    {
+        if ($this->input->post('insertar') !== NULL)
+        {
+            $reglas = $this->reglas_comunes;
+            $this->form_validation->set_rules($reglas);
+            if ($this->form_validation->run() !== FALSE)
+            {
+                $valores = $this->limpiar('insertar', $this->input->post());
+                $this->Juego->insertar($valores);
+                $this->output->delete_cache('/juegos/index');
+                redirect('juegos/index');
+            }
+        }
+        $this->template->load('juegos/insertar');
+    }
+    
+    private function limpiar($accion, $valores)
+    {
+        unset($valores[$accion]);
+        return $valores;
+    }
+    
+    public function editar($id = NULL)
+    {
+        if ($id === NULL)
+        {
+            redirect('juegos/index');
+        }
+
+        $id = trim($id);
+
+        if ($this->input->post('editar') !== NULL)
+        {
+            $reglas = $this->reglas_comunes;
+            $this->form_validation->set_rules($reglas);
+            if ($this->form_validation->run() !== FALSE)
+            {
+                $valores = $this->limpiar('editar', $this->input->post());
+                $this->Juego->editar($valores, $id);
+                $this->output->delete_cache('/juegos/index');
+                redirect('juegos/index');
+            }
+        }
+        $valores = $this->Juego->por_id($id);
+        if ($valores === FALSE)
+        {
+            redirect('juegos/index');
+        }
+        $data = $valores;
+        $this->template->load('juegos/editar', $data);
+    }
+}
---
+++ application/views/portal/index.php
@@ -0,0 +1,47 @@
+<?php template_set('title', 'Listado de artículos') ?>
+<div class="container-fluid" style="padding-top: 20px">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title">Listado de Juegos</h3>
+        </div>
+        <div class="panel-body">
+          <table border="1"
+                 class="table table-striped table-bordered table-hover table-condensed">
+            <thead>
+              <th>Descripción</th>
+              <th>Precio</th>
+              <th>Imágenes</th>
+              <th>Valoración</th>
+              <th colspan="2">Acciones</th>
+            </thead>
+            <tbody>
+              <?php foreach ($filas as $fila): ?>
+                <tr>
+                  <td><?= $fila['descripcion'] ?></td>
+                  <td><?= $fila['precio'] ?></td>
+                  <td><?= img('images/'.$fila['id'].'.jpg') ?></td>
+                  <td><?= ($fila['valoracion'] === NULL) ? 'Sin valoraciones' :
+                                                    $fila['valoracion'] ?></td>
+                  <td align="center">
+                    <?= anchor('/juegos/borrar/' . $fila['id'], 'Borrar',
+                               'class="btn btn-danger btn-xs" role="button"') ?>
+                  </td>
+                  <td align="center">
+                    <?= anchor('/juegos/editar/' . $fila['id'], 'Editar',
+                               'class="btn btn-warning btn-xs" role="button"') ?>
+                  </td>
+                </tr>
+              <?php endforeach ?>
+            </tbody>
+          </table>
+          <p align="center">
+            <?= anchor('juegos/insertar', 'Insertar',
+                       'class="btn btn-success" role="button"') ?>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
