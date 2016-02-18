11a63c47ef0fe685594c118dea3c9ed8be4f5a60
Guillermo <guillermo.lopez@iesdonana.org>
2016-02-18 20:33:01 +0100

    Solucion de conflicto eb bd.sql

--- application/sql/bd.sql
+++ application/sql/bd.sql
@@ -159,14 +159,13 @@ drop table if exists comentarios cascade;
 
 create table comentarios (
   id      bigserial       constraint pk_comentarios primary key,
-  autor   bigint          not null constraint fk_usuarios references usuarios (id),
+  autor   bigint          not null constraint fk_usuarios references usuarios (id)
+                          on update cascade on delete no action,
   contenido varchar(150)  not null,
   created_at   timestamp  not null default CURRENT_TIMESTAMP,
   autor   bigint          not null constraint fk_usuarios references usuarios (id)
                           on update cascade on delete cascade,
   contenido varchar(150)    not null,
-  fecha   date            not null default CURRENT_DATE,
-  hora    time              not null default CURRENT_TIME,
   padre_comentario   bigint  constraint fk_comentarios_padre
                           references comentarios (id) ,
   padre_juego        bigint not null constraint fk_comentarios_padre_juego
